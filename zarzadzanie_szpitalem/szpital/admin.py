dffb4877a8e375785338e2c892a5cf82a9992e5b
dhtgaming <47740790+dhtgaming@users.noreply.github.com>
2023-06-02 22:33:37 +0200

    Delete zarzadzanie_szpitalem directory

--- zarzadzanie_szpitalem/szpital/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from.models import Pacjent,Doktor,Spotkanie
-
-# Register your models here.
-admin.site.register(Doktor)
-admin.site.register(Pacjent)
-admin.site.register(Spotkanie)
